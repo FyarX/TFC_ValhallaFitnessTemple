2e5c3d11a02df63b1915ea97f3f42dc59b4c8d1b
FyarX <aaronavilamoralrob@gmail.com>
2025-03-25 19:32:03 +0100

    Subida de carpeta backend

---
+++ valhallawebsite/src/main/java/com/valhalla/valhallawebsite/ValhallawebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.valhalla.valhallawebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValhallawebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValhallawebsiteApplication.class, args);
+	}
+
+}
---
+++ valhallawebsite/src/test/java/com/valhalla/valhallawebsite/ValhallawebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valhalla.valhallawebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValhallawebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
