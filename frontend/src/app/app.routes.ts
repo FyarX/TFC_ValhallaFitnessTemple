ad7278abca1b0de3939a7525b94a28468b26d490
FyarX <aaronavilamoralrob@gmail.com>
2025-04-24 14:13:24 +0200

    Creación de estructura básica de rutas no definitiva

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,4 +1,38 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+    {
+        path: '',
+        component: LayoutMainComponent,
+        children: [
+            {
+                path: 'inicio',
+                component: HomeComponent,
+            },
+            {
+                path: 'no se aun',
+                component: NoSeAunComponent,
+            }
+        ],
+    },
+    {
+        path: 'auth',
+        component: LayoutAuthComponent,
+        children: [
+            {
+                path: 'login',
+                component: LoginComponent,
+            },
+            {
+                path: 'register',
+                component: RegisterComponent,
+            },
+        ],
+    },
+    {
+        path: 'admin',
+        component: LayoutAdminComponent,
+        children: [
+        ],
+    }
 ];
